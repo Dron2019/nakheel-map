b241b33477ae17a54f65f3825287ff9757693f27
yatsinoa <sashayatzino@gmail.com>
2023-05-06 14:52:53 +0300

    links fixed

--- src/assets/scripts/presentation/popup.js
+++ src/assets/scripts/presentation/popup.js
@@ -3,27 +3,28 @@ const buttonsRef = document.querySelectorAll('.presentation-btn');
 const links = [
   'https://www.comobynakheel.com/emotions/home',
   'https://tecma-demo.com/clients/d1west/emotions',
-  'https://nakheelmap.com/static/palm-beach-tower.pdf',
-  'https://nakheelmap.com/static/dubai-islands.pdf',
+  './web/viewer.html?file=https://nakheelmap.com/static/palm-beach-tower.pdf',
+  './web/viewer.html?file=https://nakheelmap.com/static/dubai-islands.pdf',
 ];
 
 buttonsRef.forEach(function(button) {
   button.addEventListener('click', function(event) {
     event.preventDefault();
     const popup = document.getElementById('popup');
-    const close = popup.querySelector('.presentation-popup-top__btn');
-    const iframeTitleRef = popup.querySelector('.presentation-popup-top__title');
     const index = Array.from(buttonsRef).indexOf(button);
     const iframe = popup.querySelector('iframe');
-
     iframe.src = links[index];
 
     const buttonText = button.innerText;
-
+    const iframeTitleRef = popup.querySelector('.presentation-popup-top__title');
     iframeTitleRef.innerHTML = buttonText;
+
+    // Відкриваємо попап
 
     popup.classList.add('active');
 
+    // Додаємо обробник кліків на close-елемент попапа
+    const close = popup.querySelector('.presentation-popup-top__btn');
     close.addEventListener('click', function(event) {
       event.preventDefault();
       popup.classList.remove('active');
