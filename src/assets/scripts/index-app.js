de8c96ddf73aa347cfbfb6afa0f4fdb3a39b0f8a
Dron2019 <dron_2019@ukr.net>
2023-08-09 11:45:04 +0300

    svg make contain on ipad

--- src/assets/scripts/index-app.js
+++ src/assets/scripts/index-app.js
@@ -282,13 +282,13 @@ useClickedProjectEffect(val => {
 
   if (val.name)  {
     document.body.classList.add('overlay');
-    document.querySelectorAll('[data-img-overlay]').forEach(el => el.style.opacity = '');
-    document.querySelectorAll('[data-filter_infrastructure]').forEach(el => el.style.opacity = '0');
+    document.querySelectorAll('[data-img-overlay]').forEach(el => el.style.opacity = '0');
+    // document.querySelectorAll('[data-filter_infrastructure]').forEach(el => el.style.opacity = '0');
     
   } else {
     document.body.classList.remove('overlay');
-    document.querySelectorAll('[data-img-overlay]').forEach(el => el.style.opacity = 0);
-    document.querySelectorAll('[data-filter_infrastructure]').forEach(el => el.style.opacity = '');
+    // document.querySelectorAll('[data-img-overlay]').forEach(el => el.style.opacity = 0);
+    // document.querySelectorAll('[data-filter_infrastructure]').forEach(el => el.style.opacity = '');
   }
 });
 useClickedProjectEffect(val => {
